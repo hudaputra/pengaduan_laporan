d58ec09858a82bfbbfd8481de7ad915eca4306c2
Huda Putra Santosa <hudaputrasantosa@gmail.com>
2021-04-23 15:06:01 +0700

    penambahan about

---
+++ about.php
@@ -0,0 +1,41 @@
+<?php
+include 'navbar.php';
+?>
+
+<!-- Main Content -->
+<div id="content">
+
+   <!-- Begin Page Content -->
+   <div class="container-fluid">
+
+      <!-- Page Heading -->
+      <h1 class="h3 mb-4 text-gray-800">About</h1>
+
+   </div>
+   <!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+
+
+
+</div>
+<!-- End of Content Wrapper -->
+
+</div>
+<!-- End of Page Wrapper -->
+
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+   <i class="fas fa-angle-up"></i>
+</a>
+
+<!-- Bootstrap core JavaScript-->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="js/sb-admin-2.min.js"></script>
